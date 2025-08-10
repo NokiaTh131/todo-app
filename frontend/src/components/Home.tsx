551218142f1030de43baf1a4f050504c2d4548be
monophoto <jetsadawut1013@gmail.com>
2025-08-10 13:31:13 +0700

    show cicd yellow btm

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -22,7 +22,7 @@ function Home() {
             ด้วยอินเทอร์เฟซที่ใช้งานง่ายและเรียบง่าย ให้คุณไม่พลาดทุกงานสำคัญ
           </p>
           <button
-            className="w-full red-button"
+            className="w-full yellow-button"
             data-cy="register-button"
             onClick={() => navigate("/register")}
           >
